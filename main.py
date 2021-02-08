449b2157bd09cec3f72d9eaf6fdac5a5f4e2f069
shayq <shayquinn100@gmail.com>
2021-02-08 14:03:50 +0000

    started sign up screen

---
+++ main.py
@@ -0,0 +1,29 @@
+import kivy
+kivy.require('2.0.0') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_file('design.kv')
+
+class LoginScreen(Screen):
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
+
+class RootWidget(ScreenManager):
+    pass
+
+class SignUpScreen(Screen):
+    def add_user(self,uname,pword):
+        print(uname,pword)
+        pass
+
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+if __name__ == '__main__':
+    MainApp().run()
